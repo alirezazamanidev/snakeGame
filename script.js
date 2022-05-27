5324994cca90c84eb5ace7c60248a6dbab8e6757
Alireza <zamani802000@gmail.com>
2022-05-27 14:41:35 +0430

    make move snake

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ let snake = [
 
 let foodX;
 let foody;
+let score=0;
+let dx=10;
+let dy=0;
+
+
 
 Play();
 createfood();
@@ -20,7 +25,7 @@ function Play(){
     setTimeout(()=>{
         clearCanvas()
         drawFood()
-        // advanceSnake()
+        advanceSnake()
         drawSnake();
         
         Play();
@@ -42,10 +47,29 @@ function drawSnakePart(snakePart) {
     ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
+function advanceSnake(){
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy }
+
+    snake.unshift(head);
+    if (head.x === foodX && head.y === foodY) {
+        score += 10;
+
+        document.getElementById('score').innerHTML = score;
+
+        createFood();
+    } else {
+        snake.pop()
+    }
+}
 function randomNumber(max, min) { return Math.round((Math.random() * (max - min) + min) / 10) * 10 }
 function createfood() {
     foodX = randomNumber(0, gameCanvas.width - 10);
     foodY = randomNumber(0, gameCanvas.height - 10);
+    snake.forEach(snakePart=>{
+        if(snakePart.x===foodX && snakePart.y===foodY){
+            createfood();
+        }
+    })
 
 }
 function drawFood() {
